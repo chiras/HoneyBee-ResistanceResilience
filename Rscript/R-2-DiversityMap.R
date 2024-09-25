db9e3c02ebaa998c97b2e97397654097cfc5d394
chiras <keller@bio.lmu.de>
2024-09-25 11:00:32 +0200

    added crop proportions, reshapes figures

--- Rscript/R-2-DiversityMap.R
+++ Rscript/R-2-DiversityMap.R
@@ -37,25 +37,23 @@ diversity$month[diversity$month=="08"] <- "August"
 diversity$month <- ordered(factor(diversity$month, levels = c("May", "June", "July","August")))
 
 ### plot diversity
-pdf("plots.supplement/diversity_map.pdf", width=8, height=6)
+pdf("plots.supplement/diversity_map.pdf", width=12, height=4)
 ggplot(data=diversity,aes(x=CoordX,y=CoordY, col=ExpShannon)) + geom_polygon(data = world, aes(x = long, y = lat, group = group), fill = "lightgray", colour = "white") + 
   theme_bw()+
-  geom_jitter(width=0.2, height=0.2, size=2, alpha=0.5)+
-  scale_color_viridis(option="magma") +   
+  geom_jitter(width=0.2, height=0.2, size=1.5, alpha=0.5)+
+  scale_color_viridis(option="magma", direction=-1) +   
   ylab("Latitude") + xlab("Longitude")+
-  facet_wrap(month~., ncol=2)+
+  facet_wrap(month~., ncol=4)+
     theme(panel.margin.y = unit(0, "lines"),
                                          strip.background =element_rect(fill="white"),
                                          panel.grid.minor = element_blank())+ 
-  labs(colour = bquote("Effective Diversity ("~e^H[2]~")"))
-
-
-
+  labs(colour = bquote("Effective Species Richness ("~e^H~")"))+
+    theme(legend.position = "bottom")  
 dev.off()
 
 ### plot diversity & latitiude 
 
-pdf("plots.supplement/diversity_latitude_quadratic.pdf", width=8, height=6)
+pdf("plots.supplement/diversity_latitude_quadratic.pdf", width=12, height=4)
 ggplot(diversity, aes(y=ExpShannon, x=CoordY, fill=(CoordY)))+
  geom_boxplot(aes(group=as.factor(CoordY)), linewidth=0.2, outlier.size=0.1)+
   #geom_point()+
@@ -63,19 +61,64 @@ ggplot(diversity, aes(y=ExpShannon, x=CoordY, fill=(CoordY)))+
   scale_color_viridis(discrete=F, option="turbo", direction=-1)+
   scale_fill_viridis(discrete=F, option="turbo", direction=-1)+
  scale_y_continuous(limits = c(0, 15)) +
-  theme_bw() +     facet_wrap(month~., ncol=2)+
+  theme_bw() +     facet_wrap(month~., ncol=4)+
     theme(panel.margin.y = unit(0, "lines"),
                                          strip.background =element_rect(fill="white"),
                                          panel.grid.minor = element_blank())+
 
-  ylab(bquote("Effective Diversity ("~e^H[2]~")"))+
-  xlab("Latitude")+  labs(fill = "Latitude")
+  ylab(bquote("Effective Species Richness ("~e^H~")"))+
+  xlab("Latitude")+  labs(fill = "Latitude")+
+    theme(legend.position = "bottom")  
 
 dev.off()
 
-test_results[["diversity_latitude_quadratic"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2)*Temp+Prec+as.numeric(month),data=diversity)))
+test_results[["diversity_latitude_quadratic"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2)+as.numeric(month),data=diversity)))
+#test_results[["diversity_latitude_quadratic"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2)*Temp+Prec+as.numeric(month),data=diversity)))
 test_results[["diversity_latitude_quadratic_5"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2),data=diversity[diversity$month=="May",])))
 test_results[["diversity_latitude_quadratic_6"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2),data=diversity[diversity$month=="June",])))
 test_results[["diversity_latitude_quadratic_7"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2),data=diversity[diversity$month=="July",])))
 test_results[["diversity_latitude_quadratic_8"]] <- summary(step(lm(exp(Shannon)~CoordY+I(CoordY^2),data=diversity[diversity$month=="August",])))
 options(warn = oldw)
+
+
+# proportions wild / crops
+
+crops.otu <- colSums(otu_table(samples)[taxa_names(samples) %in% crops$V1 ,])
+wild.otu <- colSums(otu_table(samples)[!(taxa_names(samples) %in% crops$V1),])
+
+cw.otu <- otu_table(t(data.frame(crops=crops.otu, wild=wild.otu)), taxa_are_rows=T)
+
+proportion <- otu_table(cw.otu[1,]/(cw.otu[1,]+cw.otu[2,]), taxa_are_rows=T)
+proportion <- merge_phyloseq(proportion, sample_data(samples))
+proportion2 <- psmelt(proportion)
+proportion2$date2 <- as.Date(proportion2$Month, format = "%d/%m/%Y")
+proportion2$month  <- format(proportion2$date2, "%m")
+
+proportion2 <- proportion2[!is.na(proportion2$month),]
+
+proportion2$month[proportion2$month=="05"] <- "May"
+proportion2$month[proportion2$month=="06"] <- "June"
+proportion2$month[proportion2$month=="07"] <- "July"
+proportion2$month[proportion2$month=="08"] <- "August"
+
+proportion2$month <- ordered(factor(proportion2$month, levels = c("May", "June", "July","August")))
+
+pdf("plots.supplement/proportion_crops.pdf", width=12, height=4)
+
+ggplot(proportion2, aes(y=Abundance, x=CoordY, fill=(CoordY)))+
+ geom_boxplot(aes(group=as.factor(CoordY)), linewidth=0.2, outlier.size=0.1)+
+  #geom_point()+
+  #geom_smooth(method = "lm", formula = y ~ x + I(x^2), col="black")+
+  scale_color_viridis(discrete=F, option="turbo", direction=-1)+
+  scale_fill_viridis(discrete=F, option="turbo", direction=-1)+
+ scale_y_continuous(limits = c(0, 1)) +
+  theme_bw() +     facet_wrap(month~., ncol=4)+
+    theme(panel.margin.y = unit(0, "lines"),
+                                         strip.background =element_rect(fill="white"),
+                                         panel.grid.minor = element_blank())+
+
+  ylab(bquote("Proportion crops (%)"))+
+  xlab("Latitude")+  labs(fill = "Latitude")+
+    theme(legend.position = "bottom")  
+
+dev.off()
